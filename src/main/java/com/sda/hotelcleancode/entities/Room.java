44e194381647ee06be9eabf66ec8dd32660013dd
Anneli Rasu <annelirasu@gmail.com>
2019-11-23 11:43:18 +0200

    Room Class written, annotations added

--- src/main/java/com/sda/hotelcleancode/entities/Room.java
+++ src/main/java/com/sda/hotelcleancode/entities/Room.java
@@ -1,4 +1,33 @@
 package com.sda.hotelcleancode.entities;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotBlank
+    private String type;
+
+    @NotBlank
+    private Integer maxPeople;
+
+    private String bedType;
+
+    @NotBlank
+    private BigDecimal price;
+
 }
