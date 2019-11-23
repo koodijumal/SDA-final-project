b4affda3d75b5abdd4a38c8bd5c77fc8d523f33d
Axel Arnus <axel.arnus@gmail.com>
2019-11-23 13:50:47 +0200

    created repositories CustomerRepository

--- src/main/java/com/sda/hotelcleancode/repositories/CustomerRepository.java
+++ src/main/java/com/sda/hotelcleancode/repositories/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.sda.hotelcleancode.repositories;
 
-public interface CustomerRepository {
+import com.sda.hotelcleancode.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Customer findCustomerByRegistryCode(String code);
 }
