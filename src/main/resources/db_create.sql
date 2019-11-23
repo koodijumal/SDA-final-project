85ed32a3d24d3b11fe5e3e507541a3fa4750ae24
Anneli Rasu <annelirasu@gmail.com>
2019-11-23 15:52:37 +0200

    Creating database again - sql code

--- src/main/resources/db_create.sql
+++ src/main/resources/db_create.sql
@@ -2,12 +2,12 @@ create table customer
 (
     id           int auto_increment
         primary key,
-    firstname    varchar(30) not null,
-    lastname     varchar(30) not null,
-    companyname  varchar(50) null,
-    registrycode varchar(50) not null,
-    constraint Customer_registrycode_uindex
-        unique (registrycode)
+    first_name    varchar(30) not null,
+    last_name     varchar(30) not null,
+    company_name  varchar(50) null,
+    registry_code varchar(50) not null,
+    constraint Customer_registry_code_uindex
+        unique (registry_code)
 );
 --just run the code
 create table reservation
@@ -17,8 +17,8 @@ create table reservation
     room         int         not null comment 'referring to room table',
     customer     int         not null comment 'referring to customer table
 ',
-    checkindate  date        not null,
-    checkoutdate date        not null,
+    checkin_date  date        not null,
+    checkout_date date        not null,
     payment      varchar(30) not null comment 'Possible values:
 not paid, paid, partially paid',
     status       varchar(30) not null comment 'possible values:
@@ -33,8 +33,8 @@ create table room
     type      varchar(10) not null comment 'Possible values:
 standard, deluxe
 ',
-    maxpeople int         not null,
-    bedtype   varchar(10) null comment 'Possible values:
+    max_people int         not null,
+    bed_type   varchar(10) null comment 'Possible values:
 twin, full',
     price     double      not null comment 'price in euros
 '
