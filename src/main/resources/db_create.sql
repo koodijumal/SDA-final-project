41fce12749103880d622aab4c02cc0cbba689ba7
Anneli Rasu <annelirasu@gmail.com>
2019-11-23 15:42:30 +0200

    Creating database again - sql code

--- src/main/resources/db_create.sql
+++ src/main/resources/db_create.sql
@@ -1,41 +1,43 @@
-create table Customer
+create table customer
 (
-  id           int auto_increment
-    primary key,
-  first_name    varchar(30) not null,
-  last_name     varchar(30) not null,
-  company_name  varchar(50) null,
-  registry_code varchar(50) not null,
-  constraint Customer_registrycode_uindex
-    unique (registry_code)
+    id           int auto_increment
+        primary key,
+    firstname    varchar(30) not null,
+    lastname     varchar(30) not null,
+    companyname  varchar(50) null,
+    registrycode varchar(50) not null,
+    constraint Customer_registrycode_uindex
+        unique (registrycode)
 );
 
-create table Reservation
+create table reservation
 (
-  id           int auto_increment
-    primary key,
-  room         int         not null comment 'referring to room table',
-  customer     int         not null comment 'referring to customer table
+    id           int auto_increment
+        primary key,
+    room         int         not null comment 'referring to room table',
+    customer     int         not null comment 'referring to customer table
 ',
-  checkin_date  date        not null,
-  checkout_date date        not null,
-  payment      varchar(30) not null comment 'Possible values:
+    checkindate  date        not null,
+    checkoutdate date        not null,
+    payment      varchar(30) not null comment 'Possible values:
 not paid, paid, partially paid',
-  status       varchar(30) not null comment 'possible values:
+    status       varchar(30) not null comment 'possible values:
 active, cancelled
 '
 );
 
-create table Room
+create table room
 (
-  id        int auto_increment
-    primary key,
-  type      varchar(10) not null comment 'Possible values:
+    id        int auto_increment
+        primary key,
+    type      varchar(10) not null comment 'Possible values:
 standard, deluxe
 ',
-  maxpeople int         not null,
-  bedtype   varchar(10) null comment 'Possible values:
+    maxpeople int         not null,
+    bedtype   varchar(10) null comment 'Possible values:
 twin, full',
-  price     double      not null comment 'price in euros
+    price     double      not null comment 'price in euros
 '
 );
+
+--uus tabel
