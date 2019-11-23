a467d91485b2204efa946fe1c4cd315c3e53555d
Anneli Rasu <annelirasu@gmail.com>
2019-11-23 10:06:43 +0200

    Initial commit

---
+++ src/main/java/com/sda/hotelcleancode/HotelcleancodeApplication.java
@@ -0,0 +1,13 @@
+package com.sda.hotelcleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelcleancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelcleancodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/hotelcleancode/HotelcleancodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.hotelcleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelcleancodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
