d53486d24ed7918e989eee7ccc28af531359754e
ankolb <ankolb@ttu.ee>
2019-11-24 11:50:18 +0200

    room added success

--- src/main/java/com/sda/hotelcleancode/repositories/ReservationRepository.java
+++ src/main/java/com/sda/hotelcleancode/repositories/ReservationRepository.java
@@ -2,6 +2,10 @@ package com.sda.hotelcleancode.repositories;
 
 import com.sda.hotelcleancode.entities.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+
 }
