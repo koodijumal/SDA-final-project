d060d42511d5acfa77e4f6c70a4e4236b82bc9e7
Axel Arnus <axel.arnus@gmail.com>
2019-11-24 13:24:57 +0200

    Updated ReservationController and ReservationService and created checkDatesFail.html and roomsAvailableSuccess.html

--- src/main/java/com/sda/hotelcleancode/controller/ReservationController.java
+++ src/main/java/com/sda/hotelcleancode/controller/ReservationController.java
@@ -6,6 +6,9 @@ import com.sda.hotelcleancode.services.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class ReservationController {
@@ -20,6 +23,17 @@ public class ReservationController {
     private ReservationService reservationService;
 
     // sisse tulevad kuup'evad, tagasta toad mis ei ole nendel kuup'evadel bronnitud
+    @PostMapping("/room/checkdates")
+    public String checkAvailability() {
+        LocalDate checkIn = LocalDate.of(2019, 11, 24);
+        LocalDate checkOut = LocalDate.of(2019, 11, 25);
+        boolean answer = reservationService.isAvailableRoom(checkIn, checkOut);
+        if (answer) {
+            return "roomsAvailableSuccess";
+        }
+        return "checkDatesFail";
+    }
+
     @GetMapping("/room/checkdates")
     public String getCheckDatePage() {
         return "checkDates";
--- src/main/java/com/sda/hotelcleancode/services/ReservationService.java
+++ src/main/java/com/sda/hotelcleancode/services/ReservationService.java
@@ -1,9 +1,16 @@
 package com.sda.hotelcleancode.services;
 
 import com.sda.hotelcleancode.entities.Reservation;
+import com.sda.hotelcleancode.entities.Room;
 import com.sda.hotelcleancode.repositories.ReservationRepository;
+import com.sda.hotelcleancode.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReservationService {
@@ -11,8 +18,68 @@ public class ReservationService {
     @Autowired
     private ReservationRepository reservationRepository;
 
+    @Autowired
+    private RoomRepository roomRepository;
+
     public Reservation addReservation(Reservation customer){
         return reservationRepository.save(customer);
     }
+
+    public boolean isAvailableRoom(LocalDate checkIn, LocalDate checkOut) {
+        // Get all reservations from table
+        List<Reservation> allReservations = reservationRepository.findAll();
+
+        // Get ID-s of all the rooms has overlapping dates with given dates
+        List<Integer> bookedRoomIDs = getBookedRoomsIDs(allReservations, checkIn, checkOut);
+
+        // Get all rooms from rooms table
+        List<Room> allRooms = roomRepository.findAll();
+
+        return !getAvailableRoomsByBookedIDs(bookedRoomIDs, allRooms).isEmpty();
+    }
+
+    public List<Room> getAvailableRoomsByDates(LocalDate checkIn, LocalDate checkOut) {
+        // Get all reservations from table
+        List<Reservation> allReservations = reservationRepository.findAll();
+
+        // Get ID-s of all the rooms has overlapping dates with given dates
+        List<Integer> bookedRoomIDs = getBookedRoomsIDs(allReservations, checkIn, checkOut);
+
+        // Get all rooms from rooms table
+        List<Room> allRooms = roomRepository.findAll();
+
+        // Return list of available rooms
+        return getAvailableRoomsByBookedIDs(bookedRoomIDs, allRooms);
+    }
+
+    private List<Room> getAvailableRoomsByBookedIDs(List<Integer> bookedRoomIDs, List<Room> allRooms) {
+        List<Room> availableRooms = new ArrayList<>();
+
+        // If room id is not in booked rooms id list then add it to the availableRooms list
+        for (Room room : allRooms) {
+            if (!bookedRoomIDs.contains(room.getId())) {
+                availableRooms.add(room);
+            }
+        }
+        return availableRooms;
+    }
+
+    private List<Integer> getBookedRoomsIDs(List<Reservation> allReservations, LocalDate checkIn, LocalDate checkOut) {
+        List<Integer> reservedIds = new ArrayList<>();
+
+        // If reservation has overlapping date with given dates then add the room id to reservedIDs list.
+        for (Reservation res: allReservations) {
+            if ((res.getCheckinDate().compareTo(checkIn) >= 0 && res.getCheckinDate().compareTo(checkOut) <= 0)
+            || (res.getCheckoutDate().compareTo(checkIn) >= 0 && res.getCheckoutDate().compareTo(checkOut) <= 0)) {
+                // if room id is not in list already then add it
+                if (!reservedIds.contains(res.getRoom())){
+                    reservedIds.add(res.getRoom());
+                }
+            }
+        }
+        return reservedIds;
+    }
+
+
 
 }
