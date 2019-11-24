cf186b2c7382cc80c41e7efb486ff5c515226770
Anneli Rasu <annelirasu@gmail.com>
2019-11-24 09:27:44 +0200

    Creating 5 rooms (hard coded) at once in RoomController.

--- src/main/java/com/sda/hotelcleancode/controller/RoomController.java
+++ src/main/java/com/sda/hotelcleancode/controller/RoomController.java
@@ -18,9 +18,21 @@ public class RoomController {
 
     @GetMapping("/room/add")
     public String addRoom() {
+
+        //Hard coded - creates 5 rooms in database, if to run localhost http://localhost:8080/room/add in browser
         Room room = new Room(RoomType.STANDARD, 2, RoomBedType.TWIN, BigDecimal.valueOf(100.0));
         roomService.addNewRoom(room);
+        Room room2 = new Room(RoomType.STANDARD, 2, RoomBedType.TWIN, BigDecimal.valueOf(100.0));
+        roomService.addNewRoom(room2);
+        Room room3 = new Room(RoomType.STANDARD, 2, RoomBedType.FULL, BigDecimal.valueOf(100.0));
+        roomService.addNewRoom(room3);
+        Room room4 = new Room(RoomType.DELUXE, 2, RoomBedType.TWIN, BigDecimal.valueOf(150.0));
+        roomService.addNewRoom(room4);
+        Room room5 = new Room(RoomType.DELUXE, 2, RoomBedType.FULL, BigDecimal.valueOf(150.0));
+        roomService.addNewRoom(room5);
         return "";
+
+        //TODO: html page will have form with login for hotel administrator for inserting new rooms (in future)
     }
 
 
