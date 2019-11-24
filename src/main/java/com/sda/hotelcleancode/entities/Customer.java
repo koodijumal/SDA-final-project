bdcd81cf03f19120a444f3c74b86d4d354713d48
Axel Arnus <axel.arnus@gmail.com>
2019-11-24 10:08:51 +0200

    Changed Customer entity class, CustomerController and CustomerService

--- src/main/java/com/sda/hotelcleancode/entities/Customer.java
+++ src/main/java/com/sda/hotelcleancode/entities/Customer.java
@@ -1,17 +1,12 @@
 package com.sda.hotelcleancode.entities;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
 public class Customer {
 
@@ -34,14 +29,14 @@ public class Customer {
     public Customer(@NotNull String firstname, @NotNull String lastname, @NotNull String registrycode) {
         this.firstName = firstname;
         this.lastName = lastname;
-        this.lastName = registrycode;
+        this.registryCode = registrycode;
     }
 
     public Customer(@NotNull String firstname, @NotNull String lastname,
                     @NotNull String registrycode, String companyname) {
         this.firstName = firstname;
         this.lastName = lastname;
-        this.lastName = registrycode;
+        this.registryCode = registrycode;
         this.companyName = companyname;
     }
 
