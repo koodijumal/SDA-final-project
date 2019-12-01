68eee1e996a56077180ae54684c7fe78cf7fa23e
Anneli Rasu <annelirasu@gmail.com>
2019-12-01 10:18:06 +0200

    Added spring-boot-starter-security to pom.xml and new sql for creating user and role tables for login building.

---
+++ src/main/resources/user_and_role_creation.sql
@@ -0,0 +1,20 @@
+create table user
+(
+    id        int auto_increment
+        primary key,
+    user_name varchar(30) not null,
+    password  varchar(10) not null,
+    role      int         not null,
+    constraint user_user_name_uindex
+        unique (user_name)
+);
+
+create table role
+(
+    id        int auto_increment
+        primary key,
+    role_name varchar(15) not null,
+    constraint role_role_name_uindex
+        unique (role_name)
+);
+
